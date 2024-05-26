1d1348572731fadc597904173d8fefb6c0962d5d
doinkythederp <doinkythederp@icloud.com>
2024-05-26 14:47:36 -0700

    feat: simplify template

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,15 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::{error::Error, time::Duration};
+use core::error::Error;
 
 use vexide::prelude::*;
 
-use crate::drivetrain::Drivetrain;
-
-mod drivetrain;
-
-struct Robot {
-    drivetrain: Drivetrain,
-    controller: Controller,
-}
+struct Robot {}
 
 impl Robot {
     fn new(peripherals: Peripherals) -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            drivetrain: Drivetrain::new(
-                Motor::new(peripherals.port_1, Gearset::Green, Direction::Forward),
-                Motor::new(peripherals.port_2, Gearset::Green, Direction::Forward),
-            ),
-            controller: peripherals.primary_controller,
-        })
+        Ok(Self {})
     }
 }
 
@@ -34,20 +21,11 @@ impl CompetitionRobot for Robot {
     type Error = Box<dyn Error>;
 
     async fn driver(&mut self) -> Result<(), Box<dyn Error>> {
-        loop {
-            let drive = self.controller.left_stick.y().unwrap_or_default() as f64;
-            let rotate = self.controller.right_stick.x().unwrap_or_default() as f64;
-
-            self.drivetrain.arcade_drive(drive, rotate)?;
-
-            sleep(Duration::from_millis(20)).await;
-        }
+        println!("Hello world!");
+        Ok(())
     }
 
     async fn autonomous(&mut self) -> Result<(), Box<dyn Error>> {
-        self.drivetrain.tank_drive(0.5, 0.5)?;
-        sleep(Duration::from_secs(2)).await;
-        self.drivetrain.tank_drive(0.0, 0.0)?;
         Ok(())
     }
 }
@@ -55,6 +33,5 @@ impl CompetitionRobot for Robot {
 #[vexide::main]
 async fn main(peripherals: Peripherals) -> Result<(), Box<dyn Error>> {
     let robot = Robot::new(peripherals)?;
-    robot.compete().await?;
-    Ok(())
+    robot.compete().await
 }
